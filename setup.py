d248e1d6a304b2822193fa2a956aec4abef40241
ipostmycodehere <huutaibkdn@gmail.com>
2021-08-08 15:12:51 +0700

    Inital commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='image_crawl',
+      version='0.1',
+      description='Crawl and transform images',
+      url='http://github.com/storborg/funniest',
+      author='Les',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
